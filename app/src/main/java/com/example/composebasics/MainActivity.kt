61b7540ac3c61ed3cadda68e662eaff104d1eed0
jun <leej1025y@live.com>
2023-04-21 13:23:52 +1000

    feat: basic counter by remember

---
+++ app/src/main/java/com/example/composebasics/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.example.composebasics
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composebasics.ui.theme.ComposeBasicsTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+//import androidx.compose.runtime.livedata.observeAsState
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApp() {
+                MyScreenContent()
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(content: @Composable () -> Unit) {
+    ComposeBasicsTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = listOf("Android", "There")) {
+    var counterState by remember{
+        mutableStateOf(0)
+    }
+    Column {
+        for (name in names) {
+            Greeting(name = name)
+            Divider()
+        }
+        Counter(
+            count = counterState,
+            updateCount = {
+                newCount -> counterState = newCount
+            }
+        )
+        if (counterState > 5){
+            Text(text = "I love to count")
+        }
+    }
+}
+
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit){
+    Button(onClick = { updateCount(count + 1) }) {
+        Text(text = "I've been clicked $count times")
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    // the modifier's order of chaining matters
+    Text(
+        text = "Hello $name!",
+        modifier = Modifier.padding(16.dp)
+    )
+}
+
+//@Preview(fontScale = 1.5f)
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApp() {
+        MyScreenContent()
+    }
+}
